58f26771d7efca69573b8dd0870e37c35254b700
Kris Logemann <krisrlogemann@gmail.com>
2019-05-14 13:31:57 -0700

    adds initial project files

---
+++ README.md
@@ -0,0 +1,6 @@
+Change Calculator WEB102. Created on May 14, 2019 by Kris Logemann.
+
+Hey everybody, thanks for checking out my code on GitHub.
+
+This webapp is my second project in WEB102 section of my curriculum at San Diego Code School. This is a change calculator, basically you input the amount of whatever the item costs, input how much money was given for said item, and hit calculate. The app will calculate how much change is due and give you the amount in dollars, quarters, dimes, nickels, and/or pennies.
+
--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 // Write your JavaScript here
+let change = {
+  totalChange: 0,
+  quarters: 0,
+  dimes: 0,
+  nickels: 0,
+  pennies: 0
+};
+
+function calculateChange() {
+  console.log("button was clicked");
+  let amountDue = document.getElementById("amount-due").value * 100;
+  let amountReceived = document.getElementById("amount-received").value * 100;
+  let subTotal = amountReceived - amountDue;
+  console.log(amountDue, amountReceived, subTotal);
+  let change$ = Math.floor(subTotal / 100);
+  document.getElementById("dollars-output").innerHTML = change$;
+  let changeQ = Math.floor((subTotal - change$ * 100) / 25);
+  document.getElementById("quarters-output").innerHTML = changeQ;
+  let changeD = Math.floor((subTotal - change$ * 100 - changeQ * 25) / 10);
+  document.getElementById("dimes-output").innerHTML = changeD;
+  let changeN = Math.floor((subTotal - change$ * 100 - changeQ * 25 - changeD * 10) / 5);
+  document.getElementById("nickels-output").innerHTML = changeN;
+  let changeP = Math.ceil((subTotal - change$ * 100 - changeQ * 25 - changeD * 10 - changeN * 5) / 1);
+  document.getElementById("pennies-output").innerHTML = changeP;
+}
